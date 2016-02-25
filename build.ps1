c3532fe1e7642beadf5fa11e5659dd174472fe36
Rohan <rkamat30@gmail.com>
2016-02-26 01:17:28 +0530

    Add ps script to create jar for CirsWeb

---
+++ build.ps1
@@ -0,0 +1,13 @@
+﻿$root=Get-Location
+$build=$root.ToString()+"\build\classes"
+$javaFiles=dir .\build\ -Recurse *.class | where {$_.FullName -notmatch 'impl'}
+ni -ItemType File manifest.txt
+jar cmf manifest.txt cirs-ejb.jar 
+foreach($jfile in $javaFiles){
+    jar uvf cirs-ejb.jar -C .\build\classes $jfile.FullName.Replace($build,'.')
+}
+
+cp cirs-ejb.jar ..\CIRSWeb\WebContent\WEB-INF\lib\cirs-ejb.jar
+Remove-Item jartmp*.tmp
+Remove-Item .\manifest.txt
+Remove-Item .\cirs-ejb.jar
